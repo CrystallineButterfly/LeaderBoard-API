ad46eb7841f2b0ac7a1f4cad600c470e35279da0
CrystallineButterfly <95975209+CrystallineButterfly@users.noreply.github.com>
2022-06-15 09:23:39 +0100

    Add styling

---
+++ docs/main.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-tabs */
+/* eslint-disable no-eval */
+/* eslint-disable camelcase */
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/** *** */ (() => { // webpackBootstrap
+// eslint-disable-next-line no-tabs
+// eslint-disable-next-line no-underscore-dangle
+// eslint-disable-next-line camelcase
+// eslint-disable-next-line no-underscore-dangle
+/** *** */ const __webpack_modules__ = ({
+
+    /***/ './src/index.js':
+    /*! **********************!*\
+  !*** ./src/index.js ***!
+  \********************* */
+    /***/ (() => {
+      eval('\n\n//# sourceURL=webpack://leader-board-api/./src/index.js?');
+      /***/ }),
+
+    /** *** */ 	});
+  /** ********************************************************************* */
+  /** *** */
+  /** *** */ 	// startup
+  /** *** */ 	// Load entry module and return exports
+  /** *** */ 	// This entry module can't be inlined because the eval devtool is used.
+  /** *** */ 	const __webpack_exports__ = {};
+  /** *** */ 	__webpack_modules__['./src/index.js']();
+/** *** */
+/** *** */ })();
